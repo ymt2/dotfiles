7c09347518c88f5f4c2d895b3152ab6389b2739f
ymt2 <tatsuya@ymt2.net>
2025-12-11 11:22:03 +0900

    emacs

--- emacs/dot-config/emacs/lisp/personal.el
+++ emacs/dot-config/emacs/lisp/personal.el
@@ -5,4 +5,16 @@
       (back-to-indentation)
     (beginning-of-line)))
 
+;; eshell
+
+(defun eshell/g (&rest args)
+    "Call magit without args, or git with args."
+    (if args
+        (eshell-command-result (concat "git " (string-join (mapcar #'shell-quote-argument args) " ")))
+      (magit-status)))
+
+(defun eshell/x ()
+  "Leave or kill the Eshell buffer, depending on `eshell-kill-on-exit'."
+  (throw 'eshell-terminal t))
+
 (provide 'personal)
